ac04c518207c66beca3f4a19f3a8af17570a680c
Bing <48603790+weibin038@users.noreply.github.com>
2019-04-05 10:56:40 -0400

    Add files via upload

--- head.php
+++ head.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title><?php echo $title ?></title>
+    <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0" />
     <meta name = "description" content = "<?php echo $content ?>">
 <!--    comes up to root directory, then gets the style for the common elements across the site - headers, fonts, etc-->
     <link rel ="stylesheet" href ="<?php echo $prefix ?>assets/css/style.css">
@@ -11,6 +12,7 @@
     <!-- common mobile responsive css -->
     <link rel = "stylesheet" href ="<?php echo $prefix ?>assets/css/mobile-responsive.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed|Roboto+Mono" rel="stylesheet">
+    <script src="<?php echo $prefix?>assets/js/mobile-responsive-touch.js"></script>
 <!--     anything else that you need; needs formatted as full <link> or <script> tag, proper path included-->
     <!-- <?php echo $extras ?> -->
     <!-- unknow what is this variable, by:weibin -->
@@ -18,4 +20,37 @@
 
 <body>
     
-<?php require "nav.php" ?>
+<nav>
+    <a href ="<?php echo $prefix ?>index.php"><img src ="<?php echo $prefix ?>assets/images/AltLogo%20(translucent%20no-tag).png" alt ="Basecamp Unix Logo" title ="Basecamp Unix"></a>
+    <div class="screen-max-570">
+        <ul>
+            <li><a href ="#">Login</a></li>
+            <li><a href ="<?php echo $prefix ?>pages/grossary.php">Glossary</a></li>
+            <li><a href ="<?php echo $prefix ?>pages/directory.php">Lessons and Quizzes</a></li>
+            <li><a href ="#">About</a></li>
+        </ul>
+    </div>
+    <!-- for screen that min 570 -->
+    <div class="screen-min-570">
+        <div class="nav">
+            <div class="burger-menu" id="burger-menu">
+                <div class="burger-menu-block">
+                    <div class="burger-float-block-right">
+                        <div class="burger-float-right">
+                            <div class="burger"></div>
+                            <div class="burger"></div>
+                            <div class="burger"></div>
+                        </div>
+                    </div>
+                    <!-- .div menu -->
+                    <div class="li">
+                        <div><a href ="#">Login</a></div>
+                        <div><a href ="<?php echo $prefix ?>pages/grossary.php">Glossary</a></div>
+                        <div><a href ="<?php echo $prefix ?>pages/directory.php">Lessons and Quizzes</a></div>
+                        <div><a href ="#">About</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
