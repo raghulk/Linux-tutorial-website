638281fbd9cd4ab1aecb9dffbdcac1da38f96fa1
Bing <48603790+weibin038@users.noreply.github.com>
2019-04-05 11:02:40 -0400

    Add files via upload

---
+++ assets/js/mobile-responsive-touch.js
@@ -0,0 +1,6 @@
+// this the function that let the mobile can have touch listen
+// window.onload = function(){
+//     document.body.addEventListener('touchstart', function(){ });
+// }
+
+document.addEventListener('touchstart', function(){ });
