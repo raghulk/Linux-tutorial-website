c2cdbb2fc450c07b18ee14e978fddea6fb6503b8
cmccullough012 <cxm1880@rit.edu>
2019-04-11 16:59:45 -0400

    beginning profile page

--- pages/page.php
+++ pages/page.php
@@ -4,7 +4,7 @@
 
     //check if they should have access
 	if(!$_SESSION['login']){
-		header('Location: index.php');
+		header('Location: login.php');
 	}
     
     $title = "Page title";
@@ -13,12 +13,41 @@
     $styleFile ="style2.css";
     $script ="pageScript.js";
 
+    if(!empty($_POST['t-email'])){
+        require $prefix.'../../../dbConnect.inc';
+        $stmt=$mysqli->prepare("INSERT INTO Login (teacher) VALUES (?)");
+		//bind
+		$stmt->bind_param("s",$_POST['t-email']);
+		//execute
+		$stmt->execute();
+        
+        $_SESSION['teacher-email']=$_POST['t-email'];
+    }
+
+    
+
     include "../head.php";
         
 ?>
+<div id ="profile-wrap">
+    <h1>Profile</h1>
+    <a href="logout.php">Logout</a>
 
-You are in, <?php echo $_SESSION['name'];?>!!!<br/>
-	<a href="logout.php">[clear the session]</a>
+    <h4>Your name:<?php echo $_SESSION['name'];?></h4>
+    <?php 
+        if (!isset($_SESSION['teacher-email']){
+            echo '<form action ="<?php echo $_SERVER[\'PHP_SELF\']; ?>" method ="POST">
+            <label for ="t-email">Teacher\'s email: </label><input id ="t-email" type ="email" name = "t-email" placeholder = "Your teacher\'s email"><br>
+            <p>If you enter a teacher\'s email, they will be notified of your score when you complete a quiz.</p>
+            </form>';
+        } else{
+            echo '<h4>Teacher\'s email:'.$_SESSION['teacher-email'].'<h4>';
+        }
+    ?>
+    <h4>Quiz Records</h4>
+
+</div>
+	
 
 
 
