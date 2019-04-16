9e6fc80f5bf975e070a95b63fcc342b73010fdef
cmccullough012 <cxm1880@rit.edu>
2019-04-16 13:09:27 -0400

    Quiz randomiztion working

--- pages/quiz1.php
+++ pages/quiz1.php
@@ -13,7 +13,7 @@
 
 //get questions
 if ($mysqli) {
-    $sql = 'select QuizID, Title from Quizzes where LessonID = "1";';
+    $sql = 'select QuestionID, Question from Questions where LessonID = "1";';
 
         $res=$mysqli->query($sql);
 
@@ -26,7 +26,7 @@ if ($mysqli) {
 
 //get answers
 if ($mysqli) {
-    $sql = 'select AnswerID, QuizID, AnswerText, Correct from Answers where LessonID = "1";';
+    $sql = 'select AnswerID, QuestionID, AnswerText, Correct from Answers where LessonID = "1";';
 
         $res=$mysqli->query($sql);
 
@@ -57,18 +57,20 @@ $a5 = [];
 
 //push answer to correct answer group
 foreach($ans as $a){
-   if($a['QuizID']==$q1['QuizID']){
+   if($a['QuestionID']==$q1['QuestionID']){
        array_push($a1, $a);
-   } else if ($a['QuizID']==$q2['QuizID']){
+   } else if ($a['QuestionID']==$q2['QuestionID']){
        array_push($a2, $a);
-   } else if ($a['QuizID']==$q3['QuizID']){
+   } else if ($a['QuestionID']==$q3['QuestionID']){
        array_push($a3, $a);
-   } else if ($a['QuizID']==$q4['QuizID']){
-       array_push($a3, $a);
-   } else if ($a['QuizID']==$q5['QuizID']){
-       array_push($a3, $a);
+   } else if ($a['QuestionID']==$q4['QuestionID']){
+       array_push($a4, $a);
+   } else if ($a['QuestionID']==$q5['QuestionID']){
+       array_push($a5, $a);
    }
 }
+
+
 
 //shuffle answers
 shuffle($a1);
@@ -76,21 +78,32 @@ shuffle($a2);
 shuffle($a3);
 shuffle($a4);
 shuffle($a5);
+
+//array of shuffled answers
 $answers =[$a1, $a2, $a3, $a4, $a5];
 
+//puts questions and answers into their HTML format 
+//@param $quest = question array holding question and ID 
+//@param $aSet = answer array holding all answers and their values
+//@param num = number of of question in page for IDs and names 
 function buildQuestion($quest, $aSet, $num){
-    $qText = $quest['Title'];
+    $qText = $quest['Question'];
     $an1 = [$aSet[0]['AnswerText'],$aSet[0]['Correct']];
     $an2 = [$aSet[1]['AnswerText'], $aSet[1]['Correct']];
     $an3 = [$aSet[2]['AnswerText'], $aSet[2]['Correct']];
     $an4 = [$aSet[3]['AnswerText'], $aSet[3]['Correct']];
+    
+    //NOTE: a value of 1 means a correct answer. 0 is wrong. These are auto populated when the question is built
     
     $html = "<p>$qText</p><input id ='A$num-1' type = 'radio' name = 'Q$num' value ='$an1[1]'><label for = 'A$num-1'>$an1[0]</label><br><input id ='A$num-2' type = 'radio' name = 'Q$num' value ='$an2[1]'><label for = 'A$num-2'>$an2[0]</label><br><input id ='A$num-3' type = 'radio' name = 'Q$num' value ='$an3[1]'><label for = 'A$num-3'>$an3[0]</label><br><input id ='A$num-4' type = 'radio' name = 'Q$num' value ='$an4[1]'><label for = 'A$num-4'>$an4[0]</label><br>";
         
     return $html;
 }
 
+//3D array pairing questions with answers 
 $QA = [[$q1,$a1],[$q2,$a2],[$q3,$a3],[$q4,$a4],[$q5,$a5]];
+
+//shuffle questions
 shuffle($QA);
 
 
@@ -101,34 +114,18 @@ shuffle($QA);
     <div id="rightColumn"> 
     <div class="content" >
         <h1> Quiz 1 </h1>
-        <div class="lesson">
-            <p>Questions go here</p>
-            <p>Question 1:</p>
-            <input id ='A1-1' type = 'radio' name = 'Q1' value ='0'><label for = 'A1-1'>1</label><br>
-            <input id ='A1-2' type = 'radio' name = 'Q1' value ='0'><label for = 'A1-2'>1</label><br>
-            <input id ='A1-3' type = 'radio' name = 'Q1' value ='0'><label for = 'A1-3'>1</label><br>
-            <input id ='A1-4' type = 'radio' name = 'Q1' value ='0'><label for = 'A1-4'>1</label><br>
+        <div class="lesson quiz-sec">
+            <form>
+                <?php 
+                    $i=1;
+                //build each question and echo
+                    foreach($QA as $qa){
+                        echo (buildQuestion($qa[0],$qa[1],$i));
+                        $i++;
+                    }
 
-            
-            <?php 
-//                $i=1;
-//                foreach($QA as $qa){
-//                    echo (buildQuestion($qa[0],$qa[1],$i));
-//                    $i++;
-//                }
-            echo (buildQuestion($q1,$a1,1));
-            echo (buildQuestion($q2,$a2,2));
-            echo (buildQuestion($q3,$a3,3));
-//            echo (buildQuestion($q4,$a4,4));
-//            echo (buildQuestion($q5,$a5,5));
-            //echo ($a1[0]['AnswerText']);
-            
-            var_dump($q4);
-            var_dump($q5);
-            var_dump($a4);
-            var_dump($a5);
-            ?>
-
+                ?>
+            </form>
         </div> <!-- end of lesson -->
         <div class ="quiz">
             <button id ="quiz-btn" type="button"><a href="#">Submit</a></button>
