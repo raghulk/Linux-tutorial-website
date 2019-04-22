285707b63294cc188ce5deece78036b0358eb83b
tafoke06 <47903058+tafoke06@users.noreply.github.com>
2019-04-22 15:29:38 -0400

    Pulls from database

--- pages/lesson2.php
+++ pages/lesson2.php
@@ -12,42 +12,25 @@ session_name("login");
     include "../head.php"; 
 
 ?>
-<div id="lesson-wrap">
-    <?php include"../assets/includes/sideMenu.php"; ?>
-    <div id="rightColumn">
-        <div class="content">
-            <h1> Lesson 2 </h1>
-            <div class="lesson">
-                <h3>Title </h3>
-                <p>Welcome to the second lesson. In this lesson you will learn a lot more of the basic commands and into so more advanced commands. These commands will basically be how you create, move and delete files and directories.<br>
 
-                    Now that you know how to find a file and directory and how to create a file or directory now you need to know how to create a directory and you do that with the command <strong> mkdir filename </strong> after mkdir you put the name of the file.<br><br>
+<?php
 
+        $sql = "SELECT * from Lesson;";
+            if($results = $mysqli->query($sql)){
 
-                    But what if you made one in the wrong file on accident? The best way to take care of that is to use <strong> rmdir filename </strong> which deletes and empty directory. Like mkdir put the filename after. If you need to delete a directory with content, you can use <strong> rm filename </strong> which will delete whatever you put next to it. If you mess up there is not an undo so be very careful as you can delete your root directory on accident. With great power comes great responsibility.<br><br>
+            }
+            else{
+                echo 'issue with query';
+                die;
+            }
 
+            if($results){
+                while($rowHolder = mysqli_fetch_array($results, MYSQLI_ASSOC)){
+                    $records[] = $rowHolder; //puts objects into array
+                }
+            }
 
-                    To move a file or directory you would use <strong> mv filename directory </strong> and this command can also be used to rename a file to do this you do <strong> mv oldfilename newfilename </strong>. Be careful though as if you already have a file with that name that file will be deleted.<br><br>
+            echo $records[2]['Content'];
+?>
 
-
-                    Now each file as permission on who can view this file. These are set into 3 groups, owner, group and all others. To change these permissions, you use <strong> chmod permissions filename </strong> where permissions is a 3-digit number with all digits being 1-7. These digits all mean something different. 0 is no permission, 1 means you can only execute the file, 2 means you can only write the file, 3 mean you can both execute and write, 4 means you can only read a file, 5 means you can read and execute, 6 means you can read and write and 7 means that you can read, write and execute the file.<br><br>
-
-
-                    To create a copy of the file you can use <strong> cp originalfilename newfilename </strong>. This means that you can create a copy of a file but be warned like with move if there is already a file with the new files name it will delete the old version.<br><br>
-
-
-                    To create a new file, you can use the <strong> touch -c filename </strong> which if the file does not exist creates and empty file or <strong> cat > filename [enter] content </strong> you must hit the enter key between filename and content. Cat is short for concatenation and touch main purpose is to change timestamps, but both are used for creating files. Either creates a new file if the file already exist touch –c will do nothing while cat > will add to the end. If you use just <strong> cat filename </strong> it will show you the content of that file. Also with cat you can combine two files with <strong> cat oldfile1 oldfile2 > newfile </strong> You can list multiple files to see multiple files at once.<br><br>
-
-
-                    Now you can go and practice these command on your own or go to take the quiz.
-                </p>
-
-            </div> <!-- end of lesson -->
-            <div class="quiz">
-                <h5>Ready to begin the quiz?</h5>
-                <button id="quiz-btn" type="button"><a href="quiz2.php">Quiz 2</a></button>
-            </div> <!-- end of quiz -->
-        </div> <!-- end of content -->
-    </div> <!-- end of right column -->
-</div>
 <?php include "../foot.php" ?>
--- pages/lesson3.php
+++ pages/lesson3.php
@@ -12,30 +12,25 @@ session_name("login");
     include "../head.php"; 
     
 ?>
-<div id="lesson-wrap">
-    <?php include"../assets/includes/sideMenu.php"; ?>
-    <div id="rightColumn">
-        <div class="content">
-            <h1> Lesson 3 </h1>
-            <div class="lesson">
-                <h3>Title</h3>
-                <p>
-                    Welcome to lesson 3
-                    This lesson will go over more advanced commands that specifically deal with reading and writing to the file and searching
-                    The first thing we will go over is what if you want to combine multiple unix commands to do this we will use the <strong> | </strong> command. This is called piping and it will give allow two commands to be mixed. An example of how it can be used is <strong> mkdir newFile | chmod 777 newFile </strong> if you remember the previous lesson this will create a new file called newFile and then change the permission of it so everyone can read write and execute this directory. Do not do this as normally you don’t want everyone to be able to access the file.
-                    Another that you want to sort a file it is very simple. You use the <strong> sort filename </strong> and that will show the lines in a file in alphabetical order. This is a great tool for sorting small things but more advanced search tools will not be covered on this site.
-                    The next part of this lesson would be the search function. To search use <strong> egrep searchCondition filename </strong>. This will allow you to search for the first character or a word in the file by using ‘’. To search for multiple words, you can use the piping command as an or. Other important characters in searches . means a single character, + means must appear one or more times, $ means they must be at the end of the line and ^ means that it must be at the beginning of the line. If you put use {n} then it must be appear n at least times and if it is listed as {n,m} means it would need to appear at least n times but not more than m times. Examples or some of these <strong> egrep $ ‘end’ filename </strong> which would mean the file would be looking for end at the end of the line.
-                    Next to learn how to get input from a file with stdin which can be written as <strong> > filename </strong>. This is where you can put an input in for an application. For instance, if you want a wordcount for a file you can use <strong> wc -w > filename </strong>. In the same way stdout is how to send an output to a file but it is used by not writing stdout but instead <strong> > filename </strong>. This will allow you to put the content of the into that file so you can connect it to a ls or an egrep to write the results to a file. An example of this would be <strong> ls >filename </strong> A warning though this will overwrite what is inside the file. Now this work great if there are no errors but if there are errors to handle those by doing <strong> 2> filename </strong>. An example of this would be <strong> ls -l 2> filename </strong>. To combine the stdout and the stderr you would have to <strong> ls -l > filename 2>&1 </strong>.
-                    That is the final lessons thank you for visiting our site and make sure to take the final quiz.
-                </p>
 
+<?php
 
-            </div> <!-- end of lesson -->
-            <div class="quiz">
-                <h5>Ready to begin the quiz?</h5>
-                <button id="quiz-btn" type="button"><a href="quiz3.php">Quiz 3</a></button>
-            </div> <!-- end of quiz -->
-        </div> <!-- end of content -->
-    </div> <!-- end of right column -->
-</div>
+        $sql = "SELECT * from Lesson;";
+            if($results = $mysqli->query($sql)){
+
+            }
+            else{
+                echo 'issue with query';
+                die;
+            }
+
+            if($results){
+                while($rowHolder = mysqli_fetch_array($results, MYSQLI_ASSOC)){
+                    $records[] = $rowHolder; //puts objects into array
+                }
+            }
+
+            echo $records[3]['Content'];
+?>
+
 <?php include "../foot.php" ?>
